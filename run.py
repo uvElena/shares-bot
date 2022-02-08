3d1f6b5065652783e838cbab854e3c88c5300a46
Elena Uvarova <olena.uvarova@gmail.com>
2022-02-08 19:04:05 +0200

    Add new share format and improve messages

--- run.py
+++ run.py
@@ -13,6 +13,8 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
+USER_SHARES = {}
+
 
 def get_curr_price():
     url = "https://marketwatch.com/investing/stock/csco"
@@ -26,8 +28,12 @@ def get_curr_price():
 
 def parse_line(str_line):
     lst = str_line.split()
-    # ['07/30/2021', '$55.0269', 'Dividend', 'Reinvestment', '1.3028']
-    return {'price': float(lst[1][1:]), 'count': float(lst[4])}
+    if len(lst) == 5:
+        # ['05/30/2015', '$12.0279', 'Dividend', 'Reinvestment', '4.3079']
+        return {'price': float(lst[1][1:]), 'count': float(lst[4])}
+    else:
+        # ['11/31/2017', '$23.77', '01/05/2023', '14']
+        return {'price': float(lst[1][1:]), 'count': float(lst[3])}
 
 
 def parse_data(str_lines):
@@ -35,21 +41,41 @@ def parse_data(str_lines):
     # return list(map(parse_line, str_lines.split('\n')))
 
 
-def calc_profit(filename, curr_price):
-    with open(filename, "r") as file:
-        shares_str = file.read().strip()
-    purchases = parse_data(shares_str)
-    profit = sum([(curr_price - p['price']) * p['count'] for p in purchases])
+def calc_profit(shares, curr_price):
+    profit = sum([(curr_price - s['price']) * s['count'] for s in shares])
     return profit
 
 
 # Telegram
 def profit(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /profit is issued."""
+    chat_id = update.message.chat.id
     curr_price = get_curr_price()
-    today_profit = calc_profit("Shares.txt", curr_price)
-    # update.message.reply_text('Your profit today is: {:.2f}'.format(today_profit))
-    update.message.reply_text(f'Your profit today is: ${today_profit:.2f}')
+
+    try:
+        today_profit = calc_profit(USER_SHARES[chat_id], curr_price)
+        update.message.reply_text(f'Your profit today is: ${today_profit:.2f}')
+        # update.message.reply_text('Your profit today is: {:.2f}'.format(today_profit))
+    except KeyError:
+        update.message.reply_text('You should update your shares')
+
+
+def update(update: Update, context: CallbackContext) -> None:
+    update_shares = update.message.text
+    chat_id = update.message.chat.id
+
+    # remove /update
+    update_shares = update_shares[len('/update'):].strip()
+
+    if update_shares == '':
+        USER_SHARES[chat_id] = []
+        update.message.reply_text('You have reseted all your shares')
+    else:
+        try:
+            USER_SHARES[chat_id] = parse_data(update_shares)
+            update.message.reply_text('You have update your shares')
+        except IndexError:
+            update.message.reply_text('Error occurred during the update')
 
 
 def main() -> None:
@@ -57,6 +83,7 @@ def main() -> None:
 
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("profit", profit))
+    dispatcher.add_handler(CommandHandler("update", update))
 
     updater.start_polling()
     updater.idle()

a6b8bc14482e944c2fa983a887c5677b5d84a0b5
Elena Uvarova <olena.uvarova@gmail.com>
2022-02-16 20:00:46 +0200

    Add start, help, show and buttons

--- run.py
+++ run.py
@@ -3,8 +3,9 @@ import requests
 from bs4 import BeautifulSoup
 import logging
 
-from telegram import Update
+from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram import ParseMode
 
 
 logging.basicConfig(
@@ -20,7 +21,8 @@ def get_curr_price():
     url = "https://marketwatch.com/investing/stock/csco"
     req = requests.get(url)
     soup = BeautifulSoup(req.content, 'html.parser')
-    # <bg-quote channel="/zigman2/quotes/209509471/composite,/zigman2/quotes/209509471/lastsale" class="value" field="Last" format="0,0.00" session="pre">63.38</bg-quote>
+    # <bg-quote channel="/zigman2/quotes/209509471/composite,/zigman2/quotes/209509471/lastsale"
+    # class="value" field="Last" format="0,0.00" session="pre">63.38</bg-quote>
     curr_price = soup.find('bg-quote', class_="value").contents
     curr_price = float(curr_price[0])
     return curr_price
@@ -28,12 +30,8 @@ def get_curr_price():
 
 def parse_line(str_line):
     lst = str_line.split()
-    if len(lst) == 5:
-        # ['05/30/2015', '$12.0279', 'Dividend', 'Reinvestment', '4.3079']
-        return {'price': float(lst[1][1:]), 'count': float(lst[4])}
-    else:
-        # ['11/31/2017', '$23.77', '01/05/2023', '14']
-        return {'price': float(lst[1][1:]), 'count': float(lst[3])}
+    # ['05/30/2015', '$12.0279', 'Dividend', 'Reinvestment', '4.3079']
+    return {'price': float(lst[1][1:]), 'count': float(lst[-1])}
 
 
 def parse_data(str_lines):
@@ -47,17 +45,50 @@ def calc_profit(shares, curr_price):
 
 
 # Telegram
+def get_keyboard():
+    reply_keyboard = [
+        ['/help', '/show', '/profit']
+    ]
+    return ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+
+
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(
+        'Welcome to Cisco Shares Bot! Select /help to see help message.',
+        reply_markup=get_keyboard()
+    )
+
+
+def help_command(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(
+        '''
+/help - show help message commands description
+/show - show current shares
+/profit - calculate your profit
+/update - update or reset shares. Example:
+```
+    /update
+    01/31/2011   $63.37   01/05/2012 13
+```''',
+        parse_mode=ParseMode.MARKDOWN,
+        reply_markup=get_keyboard()
+    )
+
+
 def profit(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /profit is issued."""
     chat_id = update.message.chat.id
     curr_price = get_curr_price()
-
     try:
         today_profit = calc_profit(USER_SHARES[chat_id], curr_price)
-        update.message.reply_text(f'Your profit today is: ${today_profit:.2f}')
-        # update.message.reply_text('Your profit today is: {:.2f}'.format(today_profit))
+        update.message.reply_text(
+            f'Your profit today is: ${today_profit:.2f}',
+            reply_markup=get_keyboard()
+        )
     except KeyError:
-        update.message.reply_text('You should update your shares')
+        update.message.reply_text(
+            'You should update your shares',
+            reply_markup=get_keyboard()
+        )
 
 
 def update(update: Update, context: CallbackContext) -> None:
@@ -69,21 +100,55 @@ def update(update: Update, context: CallbackContext) -> None:
 
     if update_shares == '':
         USER_SHARES[chat_id] = []
-        update.message.reply_text('You have reseted all your shares')
+        update.message.reply_text(
+            'You have reseted all your shares',
+            reply_markup=get_keyboard()
+        )
     else:
         try:
             USER_SHARES[chat_id] = parse_data(update_shares)
-            update.message.reply_text('You have update your shares')
+            update.message.reply_text(
+                'You have update your shares',
+                reply_markup=get_keyboard()
+            )
         except IndexError:
-            update.message.reply_text('Error occurred during the update')
+            update.message.reply_text(
+                'Error occurred during the update',
+                reply_markup=get_keyboard()
+            )
+
+
+def show(update: Update, context: CallbackContext) -> None:
+    chat_id = update.message.chat.id
+    try:
+        shares = [f"| {d['count']:<8n} | ${d['price']:.2f} |" for d in USER_SHARES[chat_id]]
+
+        table_header = f"| {'Count':<8} | Price  |"
+        separator = '+' + '-' * 10 + '+' + '-' * 8 + '+'
+        table_body = '\n'.join(shares)
+        table = '\n'.join([separator, table_header, separator, table_body, separator])
+
+        update.message.reply_text(
+            f"```\n{table}\n```",
+            parse_mode=ParseMode.MARKDOWN,
+            reply_markup=get_keyboard()
+        )
+    except KeyError:
+        update.message.reply_text(
+            'You should update your shares',
+            reply_markup=get_keyboard()
+        )
 
 
 def main() -> None:
     updater = Updater(os.environ['TOKEN'])
-
     dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help_command))
     dispatcher.add_handler(CommandHandler("profit", profit))
     dispatcher.add_handler(CommandHandler("update", update))
+    dispatcher.add_handler(CommandHandler("show", show))
 
     updater.start_polling()
     updater.idle()
